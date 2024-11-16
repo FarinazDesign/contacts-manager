f545cfbf8e464c34fc5074d268944660ceded866
Farinaz soleimani <soleimanyfarinaz71@gmail.com>
2024-11-17 01:16:32 +0330

    first commit

---
+++ src/components/Contact/Cantacts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cantacts = () => {
+    return (
+        <>
+            <p className="text-white">Contacts</p>
+        </>
+    )
+};
+
+export default Cantacts;
